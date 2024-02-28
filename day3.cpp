bf31191ac2041f11bd447b76a77e7ed1527ed1a7
Jidoex <pierre.anxionnat3@etu.univ-lorraine.fr>
2024-02-28 20:32:19 +0100

    Day 3 Part 2 over !

--- day3.cpp
+++ day3.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <optional>
 
 // DAY 3
 
@@ -25,7 +26,6 @@ bool isASymbol(char p_character)
 
     return itIs;
 }
-
 
 bool hasSymbolAtIndex(int p_charIndex, const std::string& p_previousLine, const std::string& p_currentLine, const std::string& p_nextLine)
 {
@@ -78,11 +78,119 @@ bool hasSymbolClose(int p_charIndex, const std::string& p_previousLine, const st
     return false;
 }
 
+std::optional<std::pair<int, std::pair<int, int>>> getNumber(int char_index, const std::string line)
+{
+    int number = 0;
+    int start_index = char_index;
+    int end_index = char_index;
+
+    if((char_index < 0) || (char_index >= line.size()) || !isANumber(line[char_index]))
+    {
+        return std::nullopt;
+    }
+
+    while((end_index < line.size() - 1) && isANumber(line[end_index + 1]))
+    {
+        end_index++;
+    }
+
+    while((start_index > 0) && isANumber(line[start_index - 1]))
+    {
+        start_index--;
+    }
+
+    number = std::stoi(line.substr(start_index, end_index - start_index + 1));
+
+    return {{number, {start_index, end_index}}};
+}
+
+int closeNumberResult(int p_gearIndex, const std::string& p_previousLine, const std::string& p_currentLine, const std::string& p_nextLine)
+{
+    // Check on the top
+    bool shouldTreatPreviousLine = (p_previousLine.size() != 0);
+    std::vector<int> result_vector;
+
+    if (shouldTreatPreviousLine)
+    {
+        auto result1 = getNumber(p_gearIndex - 1, p_previousLine);
+        auto result2 = getNumber(p_gearIndex, p_previousLine);
+        auto result3 = getNumber(p_gearIndex + 1, p_previousLine);
+        if(result1.has_value() && result2.has_value() && result3.has_value())
+        {
+            result_vector.push_back(result1.value().first);
+        } 
+        else if (!result2.has_value())
+        {
+            if(result1.has_value())
+            {
+                result_vector.push_back(result1.value().first);
+            }
+            if(result3.has_value())
+            {
+                result_vector.push_back(result3.value().first);
+            }
+        } 
+        else 
+        {
+            result_vector.push_back(result2.value().first);
+        }
+    }
+
+    // Check at the bottom
+    bool shouldTreatNextLine = (p_nextLine.size() != 0);
+
+    if (shouldTreatNextLine)
+    {
+        auto result1 = getNumber(p_gearIndex - 1, p_nextLine);
+        auto result2 = getNumber(p_gearIndex, p_nextLine);
+        auto result3 = getNumber(p_gearIndex + 1, p_nextLine);
+        if(result1.has_value() && result2.has_value() && result3.has_value())
+        {
+            result_vector.push_back(result1.value().first);
+        } 
+        else if (!result2.has_value())
+        {
+            if(result1.has_value())
+            {
+                result_vector.push_back(result1.value().first);
+            }
+            if(result3.has_value())
+            {
+                result_vector.push_back(result3.value().first);
+            }
+        } 
+        else 
+        {
+            result_vector.push_back(result2.value().first);
+        }
+    }
+
+    // Check on the left
+    if((p_gearIndex > 0) && isANumber(p_currentLine[p_gearIndex - 1]))
+    {
+        result_vector.push_back(getNumber(p_gearIndex - 1, p_currentLine).value().first);
+    }
+
+    // Check on the right
+    if((p_gearIndex < (p_currentLine.size() - 1)) && isANumber(p_currentLine[p_gearIndex + 1]))
+    {
+        result_vector.push_back(getNumber(p_gearIndex + 1, p_currentLine).value().first);
+    }
+    
+    std::for_each(result_vector.begin(), result_vector.end(), [](const auto & p_elem){std::cout << p_elem << " ,";});
+    std::cout << std::endl ;
+
+    return (result_vector.size() != 2) ? 0 : result_vector[0] * result_vector[1];
+}
 
 int main(int p_argv, char** p_argc) 
 {
+
+    // std::cout << closeNumberResult(3, "467..114..", "...*......", "..35..633.") << std::endl;
+    // return 0;
+
     std::fstream input_file;  
-    input_file.open("example3.txt", std::ios::in);
+    input_file.open("input_day3.txt", std::ios::in);
 
     std::cout << std::boolalpha;
     std::cout << "Day 3, lets go!" << std::endl;
@@ -155,14 +263,66 @@ int main(int p_argv, char** p_argc)
                 shouldExit=true;
             }
         } 
-
-        std::cout << "Part 1 result: " << resultPart1Sum << std::endl;
     }
     
+    input_file.close();
+    input_file.open("input_day3.txt", std::ios::in);
+    std::cout << "Part 2, lets go!" << std::endl;
+
+    // part 2
+    {
+        std::string previousLine;
+        std::string currentLine;
+        std::string nextLine;
+        std::getline(input_file, currentLine);
+        std::getline(input_file, nextLine);
+
+        bool lastLineWasParsed =false;
+        bool shouldExit =false;
+
+        int i = 0;
+        while(true)
+        {   
+            std::cout << "line " << i++ << " : ";
+
+            // Process only current line
+            {
+                std::string numberText;
+
+                for(int index=0; index < currentLine.size(); index++)
+                {
+                    if(currentLine[index] == '*')
+                    {
+                        resultPart2Sum += closeNumberResult(index, previousLine, currentLine, nextLine);
+                    }
+                }
+            }
+
+            previousLine=currentLine;
+            currentLine=nextLine;
+
+            if(shouldExit)
+            {
+                break;
+            }
+
+
+            if(!std::getline(input_file, nextLine))
+            {
+                shouldExit=true;
+            }
+
+            std::cout << std::endl;
+        }
+
+        input_file.close();
+
+
+    }
     
+    std::cout << "Part 1 result: " << resultPart1Sum << std::endl;
     std::cout << "Part 2 result: " << resultPart2Sum << std::endl;
 
-    input_file.close();
 
     return 0;
 };
